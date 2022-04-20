55f5a0f4b5fe655742338d4da8877c266f2e2d62
Wilkor <34819624+Wilkor@users.noreply.github.com>
2022-04-20 00:48:12 -0300

    Add files via upload

---
+++ precache-manifest.12c15bfae3b37e3c753f1f1d3a36fb59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7d8e35f969e09cab65335fd0785dc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5201d8b2a237580f79cf",
+    "url": "./static/css/2.dc04a4c0.chunk.css"
+  },
+  {
+    "revision": "5f67ddc6df01a2f0efb6",
+    "url": "./static/css/main.ab44527d.chunk.css"
+  },
+  {
+    "revision": "5201d8b2a237580f79cf",
+    "url": "./static/js/2.bcf08c7e.chunk.js"
+  },
+  {
+    "revision": "7895ddcdcff9b1aca87fb499ea0e4802",
+    "url": "./static/js/2.bcf08c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f67ddc6df01a2f0efb6",
+    "url": "./static/js/main.72c0e981.chunk.js"
+  },
+  {
+    "revision": "f36a3b9657e425b60302",
+    "url": "./static/js/runtime-main.637ebe85.js"
+  }
+]);
